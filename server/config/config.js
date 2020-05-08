882b232b8d8eae66956798b7e617cd579e27d43a
Arón Yáñez <aron.yanez@iaware.pro>
2020-05-08 17:16:08 -0500

    change ' per "

--- server/config/config.js
+++ server/config/config.js
@@ -11,10 +11,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 let urlDB;
 if (process.env.NODE_ENV === 'dev')
     
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = "mongodb://localhost:27017/cafe"
 
 else
-    urlDB = 'mongodb+srv://mongocafe:2xPs3v2uSSjGVzK9@cluster0-jeexb.mongodb.net/cafe?retryWrites=true&w=majority';
+    
+    urlDB = "mongodb+srv://mongocafe:2xPs3v2uSSjGVzK9@cluster0-jeexb.mongodb.net/cafe?retryWrites=true&w=majority";
 
 process.env.URLDB = urlDB;
 
